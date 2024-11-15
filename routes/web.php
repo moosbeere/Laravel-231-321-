08503f984748dd04153548c7e2499bde253bd27f
moosbeere_O <moosbeere_O@mail.ru>
2024-11-15 15:36:27 +0300

    Lab-4

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\AuthController;
- 
+use App\Http\Controllers\ArticleController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
